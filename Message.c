2950f42dc4de1e7c32110fbc9c99cfe6eb4fe67f
netoe1 <elynetobr@outlook.com>
2026-01-08 21:46:35 -0300

    add set username code safe

--- Message.c
+++ Message.c
@@ -2,17 +2,37 @@
 #ifndef MESSAGE_C
 #define MESSAGE_C
 
+#include "Utils.c"
 // Define MAX_SIZE for strings used.
 #define LIMIT_STR_USERNAME 150 
 #define LIMIT_STR_TEXT 500
 
+
+// Global username to use.
+char global_username[LIMIT_STR_USERNAME];
+
 // Define data structure
 typedef struct{
-    char username[LIMIT_STR_USERNAME];
     char text[LIMIT_STR_TEXT];
 }Message;
 
 
-void Message_setUsername(char *username);
-void Message_setText(char *text);
+void Message_setUsername(Message *ptr,char *username){
+    // Copy string to stack. 
+    
+    int result = Utils_isStrValid(global_username,LIMIT_STR_USERNAME);
+
+    if(result == 1){
+        snprintf(global_username,sizeof(global_username),"%s",username);
+        return;
+    }
+
+    perror("setUsername-err: invalid username to set.");
+}
+
+void Message_setText(Message *ptr,char *text){
+    int result = Utils_isStrValid(global_username,LIMIT_STR_USERNAME);
+}
+
+
 #endif
--- Utils.c
+++ Utils.c
@@ -1,8 +1,13 @@
 #ifndef UTILS_C
 #define UTILS_C
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <errno.h>
 #include <string.h>
 
+// Function to trim strings.
 void Utils_trimStr(char *str) {
     int inicio = 0;
     int fim = strlen(str) - 1;
@@ -26,10 +31,44 @@ void Utils_trimStr(char *str) {
     str[i] = '\0';
 }
 
+// Function to trucate buffers.
+
 void Utils_truncStr(char *str, int limite) {
     if (strlen(str) > limite) {
         str[limite] = '\0';
     }
+}
+
+
+/*
+ * Checks whether a string is valid:
+ * - is not NULL
+ * - is not empty
+ * - contains '\0' within the max_len limit
+ * - contains only printable ASCII characters
+ *
+ * Returns:
+ *  1 = valid
+ *  0 = invalid
+ */
+int Utils_isStrValid(const char *str, size_t max_len) {
+    if (str == NULL || max_len == 0)
+        return 0;
+
+    size_t i;
+
+    for (i = 0; i < max_len; i++) {
+        if (str[i] == '\0') {
+            return (i > 0);  // valid if not empty
+        }
+
+        if (!isprint((unsigned char)str[i])) {
+            return 0; // invalid character
+        }
+    }
+
+    // null terminator not found within the limit
+    return 0;
 }
 
 #endif
